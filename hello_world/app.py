2fe41bc2d3975e8965d6732d50c02145121ab1d9
Ashley Green <ashley.green@baringa.com>
2024-12-22 15:40:52 +0000

    Running subprocess

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,13 +1,14 @@
-import json
+# import json
 import os
+import subprocess
 import sys
-import requests
+# import requests
 
 # Add the submodule path to sys.path
-submodule_path = os.path.join(os.path.dirname(__file__), "hem", "src")
-sys.path.append(submodule_path)
+# submodule_path = os.path.join(os.path.dirname(__file__), "hem", "src")
+# sys.path.append(submodule_path)
 
-from hem import CsvWriter
+# from hem import CsvWriter
 
 
 def lambda_handler(event, context):
@@ -32,20 +33,33 @@ def lambda_handler(event, context):
         Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
     """
 
-    try:
-        ip = requests.get("http://checkip.amazonaws.com/")
-    except requests.RequestException as e:
-        # Send some context about this error to Lambda Logs
-        print(e)
+    # try:
+    #     ip = requests.get("http://checkip.amazonaws.com/")
+    # except requests.RequestException as e:
+    #     # Send some context about this error to Lambda Logs
+    #     print(e)
 
-        raise e
+    #     raise e
 
-    return {
-        "statusCode": 200,
-        "body": json.dumps(
-            {"message": "hello world", "location": ip.text.replace("\n", ""), "test_hem": str(CsvWriter)}
-        ),
-    }
+    # return {
+    #     "statusCode": 200,
+    #     "body": json.dumps(
+    #         {"message": "hello world", "location": ip.text.replace("\n", ""), "test_hem": str(CsvWriter)}
+    #     ),
+    # }
+
+    file_dir = os.path.dirname(__file__)
+    hem_submodule_path = os.path.join(file_dir, "hem")
+    hem_main_script_path = os.path.join(hem_submodule_path, "src", "hem.py")
+
+    subprocess.run([
+        "python",
+        hem_main_script_path,
+        hem_submodule_path + "/test/demo_files/core/demo.json",
+        "--display-progress",
+        "--epw-file",
+        file_dir + "/GBR_ENG_Eastbourne.038830_TMYx.epw"
+    ])
 
 
 if __name__ == "__main__":
