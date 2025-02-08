aa34dc455d1aa379576c30c73e07a244b0c5f2c1
Ashley Green <ashley.green@baringa.com>
2025-02-08 12:13:23 +0000

    Remove unused SAM template and related VSCode task for Terraform build

--- sam_template.tf
+++
@@ -1,9 +0,0 @@
-module "hem_lambda" {
-  source              = "terraform-aws-modules/lambda/aws"
-  version             = "~> 7.0"
-  source_path         = "hello_world/"
-  function_name       = "HelloWorldFunction"
-  handler             = "app.lambda_handler"
-  runtime             = "python3.9"
-  create_sam_metadata = true
-}
